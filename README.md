a8066c6d96f1711aeccf54f5e8f2e90598d70f04
Elio Schmutz <e.schmutz@4teamwork.ch>
2015-08-11 10:02:50 +0200

    Create package

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# ftw.contacts
-Provides a contact-contenttype
+Introduction
+============
+
+This package provides a contact contenttype.
+
+.. contents:: Table of Contents
+
+
+Installation
+============
+
+Add the package as dependency to your setup.py:
+
+.. code:: python
+
+  setup(...
+        install_requires=[
+          'ftw.contacts',
+        ])
+
+or to your buildout configuration:
+
+.. code:: ini
+
+  [instance]
+  eggs += ftw.contacts
+
+and rerun buildout.
+
+Install the Generic Setup profile.
+
+
+Links
+=====
+
+- Github: https://github.com/4teamwork/ftw.contacts
+- Issues: https://github.com/4teamwork/ftw.contacts/issues
+- Pypi: http://pypi.python.org/pypi/ftw.contacts
+- Continuous integration: https://jenkins.4teamwork.ch/search?q=ftw.contacts
+
+
+Copyright
+=========
+
+This package is copyright by `4teamwork <http://www.4teamwork.ch/>`_.
+
+``ftw.contacts`` is licensed under GNU General Public License, version 2.
---
+++ setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a1'
+maintainer = 'Mathias Leimgruber'
+
+tests_require = [
+    'ftw.builder',
+    'plone.app.testing',
+    'ftw.testbrowser'
+    ]
+
+extras_require = {
+    'tests': tests_require
+    }
+
+setup(name='ftw.contacts',
+      version=version,
+      description='Provides a contact-contenttype',
+      long_description=open(
+          'README.rst').read() + '\n' + open(
+          os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+          'Framework :: Plone',
+          'Framework :: Plone :: 4.2',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          ],
+
+      keywords='ftw plone contacts',
+      author='4teamwork AG',
+      author_email='mailto:info@4teamwork.ch',
+      maintainer=maintainer,
+      url='https://github.com/4teamwork/ftw.contacts',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', ],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+          'setuptools',
+          ],
+
+      tests_require=tests_require,
+      extras_require=extras_require,
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
