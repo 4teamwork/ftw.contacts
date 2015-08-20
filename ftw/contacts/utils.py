46e6a6783bdbe58663cc704c70b274f6880b23f2
Elio Schmutz <e.schmutz@4teamwork.ch>
2015-08-20 11:24:07 +0200

    Adjust intending

--- ftw/contacts/utils.py
+++ ftw/contacts/utils.py
@@ -129,52 +129,52 @@ class AlphabeticLetters(object):
 
 
 def generate_vcard(contact):
-        # TODO: Refactoring
-        io = StringIO()
+    # TODO: Refactoring
+    io = StringIO()
 
-        gender_map = {
-            'm': 'M',
-            'f': 'F',
-            '': 'U'
-        }
+    gender_map = {
+        'm': 'M',
+        'f': 'F',
+        '': 'U'
+    }
 
-        def addProp(name, value):
-            if not value:
-                return
-            io.write('{0}:{1}\n'.format(encode(name), encode(value)))
+    def addProp(name, value):
+        if not value:
+            return
+        io.write('{0}:{1}\n'.format(encode(name), encode(value)))
 
-        addProp('BEGIN', 'VCARD')
-        addProp('VERSION', '3.0')
-        addProp('N', '{0};{1};;{2}'.format(encode(contact.lastname),
-                                           encode(contact.firstname),
-                                           encode(contact.salutation)))
-        addProp('FN', encode(contact.title))
-        addProp('GENDER', gender_map.get(encode(contact.gender), ''))
-        addProp('ORG', encode(contact.organization))
-        addProp('ADR;TYPE=WORK', ';;{0};{1};;{2};{3}'.format(
-            encode(contact.address.replace('\n', '\\n').replace('\r', '')),
-            encode(contact.city),
-            encode(contact.postal_code),
-            encode(contact.country)))
-        addProp('EMAIL', encode(contact.email))
-        addProp('TEL;TYPE=WORK', encode(contact.phone_office))
-        addProp('TEL;TYPE=WORK;TYPE=CELL', encode(contact.phone_mobile))
-        addProp('TEL;TYPE=WORK;TYPE=FAX', encode(contact.fax))
-        addProp('URL', encode(contact.www))
-        addProp('ROLE', encode(contact.function))
-        addProp('TEL;TYPE=HOME', encode(contact.phone_private))
-        addProp('ADR;TYPE=HOME', ';;{0};{1};;{2};'.format(
-            encode(contact.address_private.replace(
-                '\r\n', '\\n').replace('\r', '')),
-            encode(contact.city_private),
-            encode(contact.postal_code_private)))
+    addProp('BEGIN', 'VCARD')
+    addProp('VERSION', '3.0')
+    addProp('N', '{0};{1};;{2}'.format(encode(contact.lastname),
+                                       encode(contact.firstname),
+                                       encode(contact.salutation)))
+    addProp('FN', encode(contact.title))
+    addProp('GENDER', gender_map.get(encode(contact.gender), ''))
+    addProp('ORG', encode(contact.organization))
+    addProp('ADR;TYPE=WORK', ';;{0};{1};;{2};{3}'.format(
+        encode(contact.address.replace('\n', '\\n').replace('\r', '')),
+        encode(contact.city),
+        encode(contact.postal_code),
+        encode(contact.country)))
+    addProp('EMAIL', encode(contact.email))
+    addProp('TEL;TYPE=WORK', encode(contact.phone_office))
+    addProp('TEL;TYPE=WORK;TYPE=CELL', encode(contact.phone_mobile))
+    addProp('TEL;TYPE=WORK;TYPE=FAX', encode(contact.fax))
+    addProp('URL', encode(contact.www))
+    addProp('ROLE', encode(contact.function))
+    addProp('TEL;TYPE=HOME', encode(contact.phone_private))
+    addProp('ADR;TYPE=HOME', ';;{0};{1};;{2};'.format(
+        encode(contact.address_private.replace(
+            '\r\n', '\\n').replace('\r', '')),
+        encode(contact.city_private),
+        encode(contact.postal_code_private)))
 
-        if contact.image:
-            imgdata = StringIO()
-            imgdata.write(stream_data(contact.image))
-            addProp('PHOTO;ENCODING=B', b64encode(imgdata.getvalue()))
+    if contact.image:
+        imgdata = StringIO()
+        imgdata.write(stream_data(contact.image))
+        addProp('PHOTO;ENCODING=B', b64encode(imgdata.getvalue()))
 
-        addProp('END', 'VCARD')
-        io.seek(0)
+    addProp('END', 'VCARD')
+    io.seek(0)
 
-        return io
+    return io
