698617f77f98c7a752a38bc8c47232cbdddd969e
Elio Schmutz <e.schmutz@4teamwork.ch>
2015-08-21 09:20:11 +0200

    JSLint contactfolder_listing.js

--- ftw/contacts/resources/contactfolder_listing.js
+++ ftw/contacts/resources/contactfolder_listing.js
@@ -1,8 +1,10 @@
 $(function() {
     ContactFolderListing.init();
 });
-var ContactFolderListing = (function($){
-    "use strict"
+var ContactFolderListing = (function($) {
+
+    'use strict';
+
     var self = {};
     var $contactsContainer;
     var $loadMoreButton;
@@ -12,64 +14,69 @@ var ContactFolderListing = (function($){
     var step = 20;
     var letter = '';
     var maxContacts = 0;
-    var searchableText = ''
+    var searchableText = '';
 
-    var init = function(){
-        $contactsContainer = $('#contact-folder-view .contactFolderContactsListig');
-        $loadMoreButton = $('#contact-folder-view .contactFolderLoadMoreContacts');
-        $searchInput = $('#contact-folder-view input#contactFolderSearchGadget');
+    var init = function() {
+        $contactsContainer = $(
+            '#contact-folder-view .contactFolderContactsListig');
+        $loadMoreButton = $(
+            '#contact-folder-view .contactFolderLoadMoreContacts');
+        $searchInput = $(
+            '#contact-folder-view input#contactFolderSearchGadget');
 
         $loadMoreButton.on('click', function(e) {reloadView();});
-        $('.contactFolderAlphabeticalSearch div.letter').on('click', function(e) { letterClick($(this)); });
+        $('.contactFolderAlphabeticalSearch div.letter').on('click', function(e) {
+            letterClick($(this));
+        });
         $searchInput.on('keyup', function() {
             updateSearch($(this).val());
         });
 
         reloadView();
-    }
+    };
     var reloadView = function() {
-        $.getJSON( "@@reload", {
+        $.getJSON('@@reload', {
                 index_from: index,
                 index_to: index + step,
                 letter: letter,
-                searchable_text: searchableText}, function( data ){
+                searchable_text: searchableText}, function(data) {
 
             // contacts
-            $contactsContainer.append( $(data.contacts) );
+            $contactsContainer.append($(data.contacts));
 
             index = index + step;
             setMoreButtonVisible(index < data.max_contacts);
 
             // letters
-            $('.contactFolderAlphabeticalSearch').html( data.letters );
+            $('.contactFolderAlphabeticalSearch').html(data.letters);
             $('.contactFolderAlphabeticalSearch div.letter').on('click', function(e) {
                 letterClick($(this));
             });
         });
-    }
-    var letterClick = function(button){
+    };
+    var letterClick = function(button) {
         var $this = $(button);
-        if ( !$this.hasClass('withContent') && !$this.hasClass('current')){
+        if (!$this.hasClass('withContent') && !$this.hasClass('current')) {
             return;
         }
         index = 0;
         $contactsContainer.empty();
 
         // Reset the letter-filter if click on the current selected letter
-        letter = ''
-        if ( !$this.hasClass('current') ){
-            letter = $this.data('key')
+        letter = '';
+        if (!$this.hasClass('current')) {
+            letter = $this.data('key');
         }
         reloadView();
-    }
-    var setMoreButtonVisible = function(setVisible){
-        if ( setVisible ){
+    };
+    var setMoreButtonVisible = function(setVisible) {
+        if (setVisible) {
             $loadMoreButton.show();
-        }else{
+        }else {
             $loadMoreButton.hide();
         }
-    }
-    var updateSearch = function(text){
+    };
+    var updateSearch = function(text) {
         index = 0;
         $contactsContainer.empty();
 
@@ -77,5 +84,5 @@ var ContactFolderListing = (function($){
         reloadView();
     };
     self.init = init;
-    return self
+    return self;
 }(jQuery));
