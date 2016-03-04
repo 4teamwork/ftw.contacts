01b3a08cc8740e140b86ee3d7c3cbebeb580fa96
Elio Schmutz <e.schmutz@4teamwork.ch>
2016-03-04 08:36:12 +0100

    Fix multiple maps

--- ftw/contacts/browser/contact.py
+++ ftw/contacts/browser/contact.py
@@ -7,6 +7,8 @@ from Products.Five.browser import BrowserView
 # Check for ftw.geo
 try:
     from collective.geo.geographer.interfaces import IGeoreferenceable
+    from collective.geo.mapwidget.browser.widget import MapWidget
+
 except ImportError:
     HAS_GEO_EXTRA = False
 else:
@@ -16,7 +18,6 @@ else:
 class ContactView(BrowserView):
     """Contact view
     """
-
     def get_memberships(self):
         return get_backreferences(self.context, IMemberBlock)
 
@@ -27,6 +28,12 @@ class ContactView(BrowserView):
         """
         """
         return HAS_GEO_EXTRA and IGeoreferenceable.providedBy(self.context)
+
+    def get_address_map(self):
+        address_map = MapWidget(self, self.request, self.context)
+        address_map.mapid = "geo-%s" % self.context.getId()
+
+        return address_map
 
 
 class ContactSummary(BrowserView):
