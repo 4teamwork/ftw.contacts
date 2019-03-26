8360239dfb4af4f7bfe042c929e50d64fee958b1
Martin Bächtold <martin@baechtold.me>
2019-03-26 17:52:01 +0100

    Only fire the event once the object has been added to the catalog.

--- ftw/contacts/sync/sync.py
+++ ftw/contacts/sync/sync.py
@@ -283,10 +283,10 @@ def sync_contacts(context, ldap_records, set_owner=False):
                 contact.manage_setLocalRoles(contact_id, ['Owner'])
                 contact.reindexObjectSecurity()
 
-            notify(ObjectAddedEvent(contact))
-
             aq_contact = context.get(contact_id)
             ct.catalog_object(aq_contact, '/'.join(aq_contact.getPhysicalPath()))
+
+            notify(ObjectAddedEvent(aq_contact))
 
             created += 1
             logger.debug("Created new contact '%s (%s)'." % (contact_id, dn))
