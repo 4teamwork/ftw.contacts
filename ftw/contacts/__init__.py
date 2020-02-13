e299e8fd429a8acec464537250bc91fa5d45796b
Daniel Jowett <d.jowett@4teamwork.ch>
2020-02-13 13:27:55 +0100

    Remove title from Plone 5's object_rename form and write a new description to match

--- ftw/contacts/__init__.py
+++ ftw/contacts/__init__.py
@@ -1,4 +1,6 @@
 from zope.i18nmessageid import MessageFactory
+import pkg_resources
 
 
 _ = MessageFactory('ftw.contacts')
+IS_PLONE_5 = pkg_resources.get_distribution('Products.CMFPlone').version >= '5'
